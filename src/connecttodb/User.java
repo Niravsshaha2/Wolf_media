0171f293f0c37e4de6ff9421495baf403b07efb8
Abhishek Firodiya <36302237+abhishjain99@users.noreply.github.com>
2023-04-05 18:37:45 -0400

    Update user.java

--- src/connecttodb/User.java
+++ src/connecttodb/User.java
@@ -7,100 +7,101 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class User {
-    public static Statement statement;
 
+  public static Statement statement;
+
+  public static void getuser(Connection connection)
+	throws SQLException {
+    String u_email_id;
+    statement = connection.createStatement();
+    do {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("");
+      System.out.println("User Login: Enter your Email: (press 0 to go back to main menu)");
+      if (sc.next() != "0") {
+	      try {
+	        u_email_id = sc.next();
+	        String x =
+	          "select u_email_id AS u_email_id from User where u_email_id='" +
+	          u_email_id +
+	          "'";
+	        ResultSet rs = statement.executeQuery(x);
 	
-	  public static void getuser(Connection connection)   throws SQLException 
-	  {  
-	        Scanner sc = new Scanner(System.in);
-	        String u_email_id;
-            statement = connection.createStatement();
-	        do {
-	            System.out.println("");
-	            System.out.println("User Login: Enter your Email");
-	            try {
-	            	u_email_id = sc.next();
-	            	u_email_id = "user1@example.com";
-	                    String x = "select u_email_id AS u_email_id from User where u_email_id='" + u_email_id+"'";
-	                    ResultSet rs4 = statement.executeQuery(x);
-	                    
-	                    if (rs4.next()) {
-	                        System.out.println("User found");
-	                        getusermenu(u_email_id,connection);
-	                        
-	                    } else{
-	                        System.out.println("Try again");
-	                        getuser(connection);
-	                    }
-	                } catch (SQLException e) {
-	                    System.out.println("Could not fetch details");
-	                    getuser(connection);
-	                }
-	        } while (true);
-	        
+	        if (rs.next()) {
+	          System.out.println("User found");
+	          getusermenu(u_email_id, connection);
+	        } else {
+	          System.out.println("Try again");
+	          getuser(connection);
+	        }
+	      } catch (SQLException e) {
+	        System.out.println("Could not fetch details");
+	        getuser(connection);
+//	      } finally {
+//	      	exitProgram(connection, statement, sc);
+		  }
+      } else {
+    	  MainMenu.displayMenu(connection);
+      }
+    } while (true);
+  }
+
+  public static void getusermenu(String u_email_id, Connection connection)
+    throws SQLException {
+    Scanner sc = new Scanner(System.in);
+    int enteredValue = 0;
+    statement = connection.createStatement();
+
+    do {
+      System.out.println("");
+      System.out.println("Select from the options below");
+      System.out.println("1. Song");
+      System.out.println("2. Aritst");
+      System.out.println("3. Album");
+      System.out.println("4. Podcast");
+      System.out.println("5. Update User Details");
+      System.out.println("0. Exit");
+      System.out.println("Enter your option:");
+      System.out.println("");
+
+      try {
+        enteredValue = sc.nextInt();
+
+        switch (enteredValue) {
+          case 1:
+            RL.getsongs(u_email_id, connection);
+            break;
+          case 2:
+            RL.getartist(u_email_id, connection);
+            break;
+          case 3:
+            RL.getalbum(u_email_id, connection);
+            break;
+          case 4:
+            PM.get_podcast(u_email_id, connection);
+            break;
+          case 5:
+            //
+            break;
+          case 0:
+            MainMenu.displayMenu(connection);
+            break;
+          default:
+            System.out.println("You have made an invalid choice. Please pick again.");
+        }
+      } catch (Exception e) {
+        System.out.println("You have made an invalid choice. Please pick again.");
+        getusermenu(u_email_id, connection);
+//      } finally {
+//    	exitProgram(connection, statement, sc);
 	  }
-	  
-	  public static void getusermenu(String u_email_id, Connection connection)   throws SQLException 
-	  {
-		  	Scanner sc = new Scanner(System.in);
-	        int enteredValue = 0;
-	        statement = connection.createStatement();
-	        
-	        do {
-	            System.out.println("");
-	            System.out.println("Select from the options below");
-	            System.out.println("1. Song");
-	            System.out.println("2. Aritst");
-	            System.out.println("3. Album");
-	            System.out.println("4. Podcast");
-	            System.out.println("5. Update User Details");
-	            System.out.println("6. Exit");
-	            System.out.println("Enter your option:");
-	            System.out.println("");
+    } while (enteredValue != 0);
+  }
 
-
-	            try {
-	                enteredValue = sc.nextInt();
-
-	                switch (enteredValue) {
-	                    case 1:
-//	                        Login.loginPage();
-//	                    	System.out.println("songsss");
-	                    	RL.getsongs(u_email_id,connection);
-	                        break;
-	                    case 2:
-	                    	RL.getartist(u_email_id,connection);
-	                        break;
-	                    case 3:
-	                    	RL.getalbum(u_email_id,connection);
-	                        break;
-	                    case 4:
-	                    	PM.getpodcast(u_email_id,connection);
-	                    	break;
-	                    case 5:
-//	                       
-	                        break;
-	                    case 6:
-	                    	MainMenu.displayMenu(connection);
-	                    	break;
-	                    default:
-	                        System.out.println("You have made an invalid choice. Please pick again.");
-	                }
-	            } catch (Exception e) {
-	                System.out.println("You have made an invalid choice. Please pick again.");
-	                getusermenu(u_email_id,connection);
-	            }
-	        } while (enteredValue != 6);
-	       
-	  }
-	  
-	  
-	  public static void exitProgram(Connection connection) {
-	        Helper.close(connection);
-	        Helper.close(statement);
-	        System.exit((0));
-	    }
-	  
-
-
+  public static void exitProgram(Connection connection, Statement statement, Scanner sc) {
+    Helper.close(connection);
+    Helper.close(statement);
+    Helper.close(sc);
+    System.exit((0));
+  }
 }
