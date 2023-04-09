68d3e8af7cc362509d79f8430d8c4fcc7447ea2e
Niravsshaha2 <111940049+Niravsshaha2@users.noreply.github.com>
2023-04-09 15:17:54 -0400

    Add files via upload

--- src/connecttodb/artist.java
+++ src/connecttodb/artist.java
@@ -7,211 +7,202 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class artist {
-
-  public static Statement statement;
-
-  public static void delete_artist_info(String rl_name, String a_email_id, Connection connection)
-    throws SQLException {
-    System.out.println("");
-    Scanner sc = new Scanner(System.in);
-    int enteredValue = 0;
-    statement = connection.createStatement();
-    String sql;
-    int rows;
-    do {
-      System.out.print("");
-      System.out.println("Select from following:");
-      System.out.println("1. Delete Artist country");
-      System.out.println("0.  Go to previous menu");
-      System.out.println("");
-
-      try {
-        enteredValue = sc.nextInt();
-
-        switch (enteredValue) {
-          case 1:
+    public static Statement statement;
+    
+    
+    public static void delete_artist_info(String rl_name, String a_email_id, Connection connection) throws SQLException{
+        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        int enteredValue = 0;
+        statement = connection.createStatement();
+        String sql;
+        int rows;
+        do{
+            System.out.print("");
+            System.out.println("Select from following:");
+            System.out.println("1. Delete Artist country");
+            System.out.println("0.  Go to previous menu");
             System.out.println("");
-            sql =
-              "update Artist set a_country = NULL WHERE a_email_id = '" +
-              a_email_id +
-              "'";
-            rows = statement.executeUpdate(sql);
-            System.out.println("Country deleted");
-            break;
+        
+        try{
+            enteredValue = sc.nextInt();
 
-          case 0:
-            RL.getRecordlabelMenu(rl_name, connection);
-            break;
+            switch (enteredValue){
+                case 1:
+                    System.out.println("");
+                    sql = "update Artist set a_country = NULL WHERE a_email_id = '"+a_email_id+"'";
+    				rows = statement.executeUpdate(sql);
+    				System.out.println("Country deleted");
+                    break;
+                case 0:
+                    RL.getRecordlabelMenu(rl_name, connection);
+    	            break;
+                default:
+                    System.out.println("You have made an invalid choice. Please pick again.");
+            }
+            } catch(Exception e){
+                System.err.println("Error: " + e.getMessage());
+            }
+        } while (enteredValue != 0);
 
-          default:
-            System.out.println(
-              "You have made an invalid choice. Please pick again."
-            );
-        }
-      } catch (Exception e) {
-        System.err.println("Error: " + e.getMessage());
-      }
-    } while (enteredValue != 0);
-  }
+    }
+    
+    public static void update_artist_info(String rl_name,String a_email_id, Connection connection) throws SQLException{
+        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        int enteredValue = 0;
+        statement = connection.createStatement();
 
-  public static void update_artist_info(String rl_name, String a_email_id, Connection connection)
-    throws SQLException {
-    System.out.println("");
-    Scanner sc = new Scanner(System.in);
-    int enteredValue = 0;
-    statement = connection.createStatement();
-
-    do {
-      System.out.print("");
-      System.out.println("Select from following:");
-      System.out.println("1. Update Artist Country");
-      System.out.println("0.  Go to previous menu");
-      System.out.println("");
-
-      try {
-        enteredValue = sc.nextInt();
-
-        switch (enteredValue) {
-          case 1:
+        do{
+            System.out.print("");
+            System.out.println("Select from following:");
+            System.out.println("1. Update Artist Country");
+            System.out.println("0.  Go to previous menu");
             System.out.println("");
-            System.out.println("Artist Country: ");
-            String a_country = sc.next();
-            String sql =
-              "UPDATE Artist SET a_country='" +
-              a_country +
-              "' WHERE a_email_id = '" +
-              a_email_id +
-              "'";
-            int rows = statement.executeUpdate(sql);
-            System.out.println("Country updated");
-            break;
+        
+        try{
+            enteredValue = sc.nextInt();
 
-          case 0:
-            RL.getRecordlabelMenu(rl_name, connection);
-            break;
+            switch (enteredValue){
+                case 1:
+                    System.out.println("");
+                    System.out.println("Artist Country: ");
+                    String a_country = sc.next();
+    				String sql = "UPDATE Artist SET a_country='"+a_country+"' WHERE a_email_id = '"+a_email_id+"'";
+    				int rows = statement.executeUpdate(sql);
+    				System.out.println("Country updated");
 
-          default:
-            System.out.println(
-              "You have made an invalid choice. Please pick again."
-            );
-        }
-      } catch (Exception e) {
-        System.err.println("Error: " + e.getMessage());
-      }
-    } while (enteredValue != 0);
-  }
+                    break;
 
-  public static void viewartist(String rl_name, Connection connection)
-    throws SQLException {
-    String query;
-    statement = connection.createStatement();
-    ResultSet rs = null;
+                case 0:
+                	RL.getRecordlabelMenu(rl_name, connection);
+                    break;
+                default:
+                    System.out.println("You have made an invalid choice. Please pick again.");
+            	}
+            } catch(Exception e){
+                System.err.println("Error: " + e.getMessage());
+            }
+        } while (enteredValue != 0);
 
-    System.out.println("");
-    System.out.println("List of Artists");
-    System.out.println("");
-    //print all columns
-
-    query =
-      "SELECT a_name,a_email_id FROM Artist where rl_name ='" + rl_name + "'";
-
-    try {
-      rs = statement.executeQuery(query);
-      int row = 1;
-
-      while (rs.next()) {
-        String title = rs.getString("a_name");
-        System.out.println(row + " " + title + "");
-        row++;
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
     }
-  }
+    
+    
+    public static void viewartist(String rl_name,Connection connection) throws SQLException
 
-  public static void add_artist_info(String rl_name, Connection connection)
-    throws SQLException {
-    System.out.println("");
-    System.out.println("Enter Artist details: ");
+    {
+    	String query;
+        statement = connection.createStatement();
+        ResultSet rs = null;
 
-    Statement stmt = connection.createStatement();
+    	 System.out.println("");
+  		  System.out.println("List of Artists");
+  		  System.out.println("");
+  		  //print all columns 
+  		  
+	      
+	  		  query = "SELECT a_name,a_email_id FROM Artist where rl_name ='" + rl_name +"'";
 
-    try {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Enter Artist Email ID:");
-      String a_email_id = scanner.next();
+	      
 
-      System.out.println("Enter Artist Name:");
-      scanner.nextLine();
-      String a_name = scanner.nextLine();
+  	        try {
+  	            rs = statement.executeQuery(query);
+  	            int row = 1;
 
-      System.out.println("Enter Country:");
-      String a_country = scanner.next();
-
-      System.out.println("Enter Primary Genre:");
-      String a_primary_genre = scanner.next();
-      String sql;
-
-      sql =
-        "INSERT INTO Artist (a_email_id, a_name, a_status, a_country, a_primary_genre, rl_name) VALUES ('" +
-        a_email_id +
-        "', '" +
-        a_name +
-        "', 'ACTIVE', '" +
-        a_country +
-        "', '" +
-        a_primary_genre +
-        "', '" +
-        rl_name +
-        "')";
-
-      int rows = stmt.executeUpdate(sql);
-      System.out.println(rows + " row(s) inserted.");
-    } catch (Exception e) {
-      System.err.println("Error: " + e.getMessage());
+  	                while(rs.next()){
+  	                   String title = rs.getString("a_name");
+  	                    System.out.println(row + " " +title + "" );
+  	                    row++;
+  	                }
+  	     } catch (SQLException e) {
+ 	            e.printStackTrace();
+ 	        }
     }
-  }
+    
+    
+    public static void add_artist_info(String rl_name,Connection connection) throws SQLException
+ 	{
+ 		System.out.println("");
+		System.out.println("Enter Artist details: ");
 
-  //	  Get artist list
-  public static void getartist(String u_email_id, Connection connection)
-    throws SQLException {
-    System.out.println("");
-    System.out.println("List of Artists");
-    System.out.println("");
+        Statement stmt = connection.createStatement();
+ 	
+ 		 try {
+             Scanner scanner = new Scanner(System.in);
+             System.out.println("Enter Artist Email ID:");
+             String a_email_id = scanner.next();
+             
+             System.out.println("Enter Artist Name:");
+             scanner.nextLine();
+             String a_name = scanner.nextLine();
+               
+             System.out.println("Enter Country:");
+             String a_country = scanner.next();
+             
+             System.out.println("Enter Primary Genre:");
+             String a_primary_genre = scanner.next();
+             String sql;
+           
+              sql = "INSERT INTO Artist (a_email_id, a_name, a_status, a_country, a_primary_genre, rl_name) VALUES ('" + a_email_id + "', '" + a_name + "', 'ACTIVE', '" + a_country + "', '" + a_primary_genre + "', '" + rl_name + "')";
+             
+             int rows = stmt.executeUpdate(sql);
+             System.out.println(rows + " row(s) inserted.");
 
-    statement = connection.createStatement();
-    Scanner sc = new Scanner(System.in);
-    String query, title, s_id = "";
+         } catch (Exception e) {
+             System.err.println("Error: " + e.getMessage());
+         }
+ 	}
+	
+//	  Get artist list
+	  public static void getartist(String u_email_id, Connection connection)   throws SQLException 
+	  {
+		  System.out.println("");
+		  System.out.println("List of Artists");
+		  System.out.println("");
+		  
+		  
+		  statement = connection.createStatement();
+	      Scanner sc = new Scanner(System.in);
+	      String query, title,s_id ="";
+	
+		  query = "SELECT a_name,a_email_id FROM Artist";
 
-    query = "SELECT a_name,a_email_id FROM Artist";
+	        ResultSet rs = null;
+	        try {
+	            rs = statement.executeQuery(query);
+	            int row = 1;
 
-    ResultSet rs = null;
-    try {
-      rs = statement.executeQuery(query);
-      int row = 1;
+	                while(rs.next()){
+	                    title = rs.getString("a_name");
+	                    System.out.println(row + " " +title + "" );
+	                    row++;
+	                }
+	            
+	          		System.out.println("");
+	            	System.out.println("Enter Artist id to select");
+	            	System.out.println("");	            
 
-      while (rs.next()) {
-        title = rs.getString("a_name");
-        System.out.println(row + " " + title + "");
-        row++;
-      }
+	            int selection = sc.nextInt();
+	            rs.absolute(selection - 1);
+	            if(rs.next())
+	            {
+	            String artist_name = rs.getString("a_email_id");
+//	            System.out.println("Selected value: " + artist_name);
+	            song.getsongs("Artist",artist_name, u_email_id,connection);
 
-      System.out.println("");
-      System.out.println("Enter Artist id to select");
-      System.out.println("");
+	            }
+	            else
+	            {
+	            	getartist(u_email_id, connection);
+	            }
 
-      int selection = sc.nextInt();
-      rs.absolute(selection - 1);
-      if (rs.next()) {
-        String artist_name = rs.getString("a_email_id");
-        // System.out.println("Selected value: " + artist_name);
-        song.getsongs("Artist", artist_name, u_email_id, connection);
-      } else {
-        getartist(u_email_id, connection);
-      }
-    } catch (SQLException e) {
-      Helper.close(rs);
-      e.printStackTrace();
-    }
-  }
+
+	        }
+	        catch (SQLException e) {
+		            Helper.close(rs);
+		            e.printStackTrace();
+		        }
+	                  
+	   
+	  }
 }
--- src/connecttodb/reports.java
+++ src/connecttodb/reports.java
@@ -7,146 +7,165 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class reports {
+    public static Statement statement;
+    public static void MPC_artist(Connection connection) throws SQLException
+    {
+		  System.out.println("");
+		  System.out.println("List of Artist and its monthly count");
+		  System.out.println("");
 
-  public static Statement statement;
+	      statement = connection.createStatement();
+	      String query, a_email_id,Artist_play_count;
+	      
+		  query = "Select a_email_id,sum(ls_play_count) as Artist_play_count from sings natural join listens_to_song group by a_email_id;";
 
-  public static void MPC_artist(Connection connection) throws SQLException {
-    System.out.println("");
-    System.out.println("List of Artist and its monthly count");
-    System.out.println("");
+	        ResultSet rs = null;
+	        try {
+	            rs = statement.executeQuery(query);
+	                while(rs.next()){
+	                	a_email_id = rs.getString("a_email_id");
+	                	Artist_play_count = rs.getString("Artist_play_count");
 
-    statement = connection.createStatement();
-    String query, a_email_id, Artist_play_count;
+	                    System.out.println(a_email_id + "  " + Artist_play_count);
+	                }
+                  System.out.println();
 
-    query =
-      "Select a_email_id,sum(ls_play_count) as Artist_play_count from sings natural join listens_to_song group by a_email_id;";
-
-    ResultSet rs = null;
-    try {
-      rs = statement.executeQuery(query);
-      while (rs.next()) {
-        a_email_id = rs.getString("a_email_id");
-        Artist_play_count = rs.getString("Artist_play_count");
-
-        System.out.println(a_email_id + "  " + Artist_play_count);
-      }
-      System.out.println();
-    } catch (SQLException e) {
-      Helper.close(rs);
-      e.printStackTrace();
+	        }
+	        catch (SQLException e) {
+	    	            Helper.close(rs);
+	    	            e.printStackTrace();
+	    	        }
     }
-  }
 
-  public static void MPC_album(Connection connection) throws SQLException {
-    System.out.println("");
-    System.out.println("List of Album and its monthly count");
-    System.out.println("");
+    public static void MPC_album(Connection connection) throws SQLException
+    {
+		  System.out.println("");
+		  System.out.println("List of Album and its monthly count");
+		  System.out.println("");
 
-    statement = connection.createStatement();
-    String query, l_name, l_play_count;
+	      statement = connection.createStatement();
+	      String query, l_name,l_play_count;
+	      
+		  query = "Select sum(l.ls_play_count) as l_play_count,s_title,l_name from Song s join listens_to_song l on s.s_id=l.s_id group by l_name;";
 
-    query =
-      "Select sum(l.ls_play_count) as l_play_count,s_title,l_name from Song s join listens_to_song l on s.s_id=l.s_id group by l_name;";
+	        ResultSet rs = null;
+	        try {
+	            rs = statement.executeQuery(query);
+	                while(rs.next()){
+	                    l_name = rs.getString("l_name");
+	                    l_play_count = rs.getString("l_play_count");
 
-    ResultSet rs = null;
-    try {
-      rs = statement.executeQuery(query);
-      while (rs.next()) {
-        l_name = rs.getString("l_name");
-        l_play_count = rs.getString("l_play_count");
+	                    System.out.println(l_name + "  " + l_play_count);
+	                }
+                    System.out.println();
 
-        System.out.println(l_name + "  " + l_play_count);
-      }
-      System.out.println();
-    } catch (SQLException e) {
-      Helper.close(rs);
-      e.printStackTrace();
+	        }
+	        catch (SQLException e) {
+	    	            Helper.close(rs);
+	    	            e.printStackTrace();
+	    	        }
     }
-  }
+ 	public static void MPC_song(Connection connection) throws SQLException
+    {
+		  System.out.println("");
+		  System.out.println("List of Songs and its monthly count");
+		  System.out.println("");
 
-  public static void MPC_song(Connection connection) throws SQLException {
-    System.out.println("");
-    System.out.println("List of Songs and its monthly count");
-    System.out.println("");
+	      statement = connection.createStatement();
+	      String query, title,s_id, ls_play_count;
+	      
+		  query = "Select s.s_id as s_id,l.ls_play_count as ls_play_count,s_title from Song s join listens_to_song l on s.s_id=l.s_id group by s_id";
 
-    statement = connection.createStatement();
-    String query, title, s_id, ls_play_count;
+	        ResultSet rs = null;
+	        try {
+	            rs = statement.executeQuery(query);
+	                while(rs.next()){
+	                    title = rs.getString("s_title");
+	                    s_id = rs.getString("s_id");
+	                    s_id = rs.getString("s_id");
+	                    ls_play_count = rs.getString("ls_play_count");
 
-    query =
-      "Select s.s_id as s_id,l.ls_play_count as ls_play_count,s_title from Song s join listens_to_song l on s.s_id=l.s_id group by s_id";
+	                    System.out.println(s_id + "-> " +title + "  " + ls_play_count);
+	                }
+                    System.out.println();
 
-    ResultSet rs = null;
-    try {
-      rs = statement.executeQuery(query);
-      while (rs.next()) {
-        title = rs.getString("s_title");
-        s_id = rs.getString("s_id");
-        s_id = rs.getString("s_id");
-        ls_play_count = rs.getString("ls_play_count");
-
-        System.out.println(s_id + "-> " + title + "  " + ls_play_count);
-      }
-      System.out.println();
-    } catch (SQLException e) {
-      Helper.close(rs);
-      e.printStackTrace();
+	        }
+	        catch (SQLException e) {
+	    	            Helper.close(rs);
+	    	            e.printStackTrace();
+	    	        }
     }
-  }
+ 	
+ 	
+ 	public static void getreportsmenu(Connection connection) throws SQLException
+ 	{
+ 		 System.out.println("");
+         Scanner sc = new Scanner(System.in);
+         int enteredValue = 0;
+         statement = connection.createStatement();
+         String sql;
+         int rows;
+         do{
+             System.out.print("");
+             System.out.println("Select what you want to update:");
+             System.out.println("1. Monthly play count per Song");
+             System.out.println("2. Monthly play count per Artist");
+             System.out.println("3. Monthly play count per Album");
+             
+             System.out.println("4. Report all songs given an artist");
+             System.out.println("5. Report all songs given an album");
+             
+             System.out.println("6. total payments made out to host per a given time period");
+             System.out.println("7. total payments made out to artist per a given time period");
+             System.out.println("8. total payments made out to Record Label per a given time period");
 
-  public static void getreportsmenu(Connection connection) throws SQLException {
-    System.out.println("");
-    Scanner sc = new Scanner(System.in);
-    int enteredValue = 0;
-    statement = connection.createStatement();
-    String sql;
-    int rows;
-    do {
-      System.out.print("");
-      System.out.println("Select what you want to update:");
-      System.out.println("1. Monthly play count per Song");
-      System.out.println("2. Monthly play count per Artist");
-      System.out.println("3. Monthly play count per Album");
+             System.out.println("9. Total revenue of the streaming service per month");
+             System.out.println("10. Total revenue of the streaming service per year");
 
-      System.out.println("4. Report all songs given an artist");
-      System.out.println("5. Report all songs given an album");
+             System.out.println("0.  Go to previous menu");
+             System.out.println("");
+         
+         try{
+             enteredValue = sc.nextInt();
 
-      System.out.println("0.  Go to previous menu");
-      System.out.println("");
+             switch (enteredValue){
+                 case 1:
+                    reports.MPC_song(connection);
+                     break;
+                 case 2:
+                	 reports.MPC_artist(connection);
+                     break;
+                 case 3:
+                	 reports.MPC_album(connection);
+                	 break;
+                 case 4:
+                	 artist.getartist("",connection);
+                	 break;
+                 case 5:
+                	 album.getalbum("",connection);
+                	 break;
+                 case 6:
+                	 break;
+                 case 7:
+                	 break;
+                 case 8:
+                	 break;
+                 case 9:
+                	 break;
+                 case 10:
+                	 break;
+                	 	 
+                 case 0:
+                     admin.getAdminMenu(connection);
+     	            break;
+                 default:
+                     System.out.println("You have made an invalid choice. Please pick again.");
+             }
+             } catch(Exception e){
+                 System.err.println("Error: " + e.getMessage());
+             }
+         } while (enteredValue != 0);
+ 	}
 
-      try {
-        enteredValue = sc.nextInt();
 
-        switch (enteredValue) {
-          case 1:
-            reports.MPC_song(connection);
-            break;
-
-          case 2:
-            reports.MPC_artist(connection);
-            break;
-
-          case 3:
-            reports.MPC_album(connection);
-            break;
-
-          case 4:
-            artist.getartist("", connection);
-            break;
-
-          case 5:
-            album.getalbum("", connection);
-            break;
-
-          case 0:
-            admin.getAdminMenu(connection);
-            break;
-
-          default:
-            System.out.println("You have made an invalid choice. Please pick again.");
-        }
-      } catch (Exception e) {
-        System.err.println("Error: " + e.getMessage());
-      }
-    } while (enteredValue != 0);
-  }
 }
